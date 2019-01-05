f62225c975d205f09ee1bd2da1941e5c5067afa8
Juan Je Garcia <juanjegarcialopez@gmail.com>
2019-01-05 02:41:31 -0600

    run prettier

--- screens/Restaurants/index.js
+++ screens/Restaurants/index.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import {
-  View,
-  ActivityIndicator,
-  Text,
-  SafeAreaView,
-} from 'react-native';
+import React from "react";
+import { View, ActivityIndicator, Text, SafeAreaView } from "react-native";
 
-import { Query } from 'react-apollo';
-import { RESTAURANT_SEARCH_QUERY } from '../../graphql/queries';
+import { Query } from "react-apollo";
+import { RESTAURANT_SEARCH_QUERY } from "../../graphql/queries";
+import NavBar from "../../components/NavBar";
 
 export default class Restaurants extends React.Component {
-  state = {
-  };
+  state = {};
 
   render() {
     // TODO: This shouldn't be hard coded.  Allow the user to enter it into a Text Input
-    const address = '1260 6th Ave, New York, NY 10020';
+    const address = "1260 6th Ave, New York, NY 10020";
 
     return (
-      <SafeAreaView style={{flex: 1, backgroundColor: '#fff'}}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+        <NavBar />
         <Query
           query={RESTAURANT_SEARCH_QUERY}
           variables={{
-            address
+            address,
           }}
         >
           {({ loading, error, data = {} }) => {
             if (loading) {
               return (
-                <View style={{ width: '100%', paddingVertical: 10 }}>
+                <View style={{ width: "100%", paddingVertical: 10 }}>
                   <ActivityIndicator size="large" style={{ padding: 30 }} />
                 </View>
               );
             }
 
-            console.log('DO SOMETHING SMART WITH THIS DATA');
+            console.log("DO SOMETHING SMART WITH THIS DATA");
             //console.log('data', data);
             //console.log('error', error);
 
             // Make sure we have data
-            if (data.search_restaurants && data.search_restaurants.results && data.search_restaurants.results.length > 0) {
+            if (
+              data.search_restaurants &&
+              data.search_restaurants.results &&
+              data.search_restaurants.results.length > 0
+            ) {
               return (
-                <View style={{ width: '100%', paddingVertical: 10 }}>
-                  <Text>Num Restaurants: {data.search_restaurants.results.length}</Text>
+                <View style={{ width: "100%", paddingVertical: 10 }}>
+                  <Text>
+                    Num Restaurants: {data.search_restaurants.results.length}
+                  </Text>
                   <Text>----</Text>
-                  {data.search_restaurants.results.map((r) => {
-                    return (
-                      <Text key={r.id}>{r.title}</Text>
-                    );
+                  {data.search_restaurants.results.map(r => {
+                    return <Text key={r.id}>{r.title}</Text>;
                   })}
                 </View>
               );
@@ -55,7 +55,7 @@ export default class Restaurants extends React.Component {
 
             // No Data Return
             return (
-              <View style={{ width: '100%', paddingVertical: 10 }}>
+              <View style={{ width: "100%", paddingVertical: 10 }}>
                 <Text>No Results</Text>
               </View>
             );
@@ -65,7 +65,3 @@ export default class Restaurants extends React.Component {
     );
   }
 }
-
-const styles = {
-};
-
